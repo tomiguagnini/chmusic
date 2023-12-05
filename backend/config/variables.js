d0bc9c0224ec81d311c3d136c8e4700352546c69
unknown <tomyguagnini@gmail.com>
2023-12-05 19:14:57 -0300

    add: DB_URL env

--- backend/config/variables.js
+++ backend/config/variables.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const apiKey = process.env.API_KEY
 const clientUrl = process.env.CLIENT_URL
 const app_host = process.env.APP_HOST 
-const db_url = `${process.env.DB_CLIENT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+const db_url = process.env.DB_URL || `${process.env.DB_CLIENT}://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
 module.exports = {
     apiKey,
