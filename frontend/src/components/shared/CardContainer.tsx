64a7f242f7a324ab90bd151a927c7c42510e0022
unknown <tomyguagnini@gmail.com>
2023-12-06 01:46:41 -0300

    remove default value Song

--- frontend/src/components/shared/CardContainer.tsx
+++ frontend/src/components/shared/CardContainer.tsx
@@ -1,40 +1,26 @@
-import CardShop from "@/components/shared/CardShop"
+import CardShop from "@/components/shared/CardShop";
 import { getSongs } from "@/services";
 import { Song } from "@/types";
-import { useState, useEffect } from 'react'
-
-
+import { useState, useEffect } from "react";
 
 function CardContainer() {
-    const [songs, setSongs] = useState<Array<Song>>([
-      {
-        ID:0,
-        Title:'secuen',
-        Artist:'ch',
-        Price:'100',
-        Image:'/assets/imagen0.jpeg',
-        Listening:'/assets/algo.mp3',
-        createdAt:'asd',
-        updatedAt:'asd'
-      }
-    ]);
+    const [songs, setSongs] = useState<Array<Song>>([]);
     useEffect(() => {
         getSongs()
-        .then((r) => setSongs(r.data))
-        .catch((error)=> console.log(error))
+            .then((r) => setSongs(r.data))
+            .catch((error) => console.log(error));
     }, []);
     if (!Array.isArray(songs)) {
-      // Si songs no es una matriz, puedes manejarlo de alguna manera, como asignar un valor predeterminado o mostrar un mensaje de error.
-      return <div>Error: No se pudo cargar la lista de canciones.</div>;
+        // Si songs no es una matriz, puedes manejarlo de alguna manera, como asignar un valor predeterminado o mostrar un mensaje de error.
+        return <div>Error: No se pudo cargar la lista de canciones.</div>;
     }
-  return (
-    <div className="flex gap-15 flex-wrap justify-center w-full">
-        {songs.map((s)=>{
-          return (<CardShop song={s} key={s.ID}></CardShop>)
-          
-        })}
-    </div>
-  )
+    return (
+        <div className="flex gap-15 flex-wrap justify-center w-full">
+            {songs.map((s) => {
+                return <CardShop song={s} key={s.ID}></CardShop>;
+            })}
+        </div>
+    );
 }
 
-export default CardContainer
+export default CardContainer;
