1ebeb7c9b49b1d309a7ab2f9f2ae1a3bfb5886ce
unknown <tomyguagnini@gmail.com>
2023-12-06 01:07:31 -0300

    error render vercel

--- frontend/src/components/shared/CardContainer.tsx
+++ frontend/src/components/shared/CardContainer.tsx
@@ -23,6 +23,10 @@ function CardContainer() {
         .then((r) => setSongs(r.data))
         .catch((error)=> console.log(error))
     }, []);
+    if (!Array.isArray(songs)) {
+      // Si songs no es una matriz, puedes manejarlo de alguna manera, como asignar un valor predeterminado o mostrar un mensaje de error.
+      return <div>Error: No se pudo cargar la lista de canciones.</div>;
+    }
   return (
     <div className="flex gap-15 flex-wrap justify-center w-full">
         {songs.map((s)=>{
