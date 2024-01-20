c1d9dce6f6f4970e2cb6bd8967727d6741de2d20
unknown <tomyguagnini@gmail.com>
2024-01-20 18:08:59 -0300

    translate items license

--- frontend/src/components/shared/License.tsx
+++ frontend/src/components/shared/License.tsx
@@ -15,7 +15,7 @@ import {
     CardTitle,
   } from "@/components/ui/card"
 import { Button } from "../ui/button";
-import { Layers, Mic, Mic2, RadioTower, Video } from "lucide-react";
+import { BoomBox, Layers, Mic, Mic2, RadioTower, ShoppingCart, Video } from "lucide-react";
 import { Song } from "@/types";
 import {  MouseEventHandler, ReactNode } from "react";
 
@@ -40,15 +40,18 @@ export default function License({  song, children, onClick }: props) {
                         <Card>
                             <CardHeader className="flex flex-row items-center justify-between">
                                 <CardTitle>Licencia Estandar</CardTitle>
-                                <Button variant={"outline"} className="bg-primary-600" onClick={onClick}>$ {song.Price}</Button>
+                                <Button variant={"outline"} className="bg-primary-600" onClick={onClick}>
+                                    $ {song.Price} 
+                                    </Button>
                                 
                             </CardHeader>
-                            <CardContent className="grid-cols-2 ">
-                                <p className="flex gap-3 text-sm"><span><Mic/></span>used for Music Recording</p>
-                                <p className="flex gap-3 text-sm"><span><RadioTower/></span>80.000 audio streams</p>
-                                <p className="flex gap-3 text-sm"><span><Mic2/></span>for profit audio performance</p>
-                                <p className="flex gap-3 text-sm"><span><Layers/></span>distribute 50 copies</p>
-                                <p className="flex gap-3 text-sm"><span><Video/></span>1 music video</p>
+                            <CardContent className="flex flex-col">
+                                <p className="flex gap-3 text-sm"><span><Mic/></span>Utilizado para grabacion de musica</p>
+                                <p className="flex gap-3 text-sm"><span><RadioTower/></span>80.000 transmicion de audio en linea</p>
+                                <p className="flex gap-3 text-sm"><span><Mic2/></span>Actuaciones en vivo con fines de lucro</p>
+                                <p className="flex gap-3 text-sm"><span><BoomBox/></span>Derechos de radiodifucion 5 (cinco)</p>
+                                <p className="flex gap-3 text-sm"><span><Layers/></span>Distribucion de 2000 copias</p>
+                                <p className="flex gap-3 text-sm"><span><Video/></span>2 videos musicales</p>
                             </CardContent>
                             <CardFooter>
                                 
